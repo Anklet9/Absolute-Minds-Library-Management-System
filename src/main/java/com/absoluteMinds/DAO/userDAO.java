7422f823bbdf51be13b24b9e486f6df81190b811
Aniket Sengar <75060938+Anklet9@users.noreply.github.com>
2023-10-13 16:46:30 +0530

    Final
    All issues have been closed

--- src/main/java/com/absoluteMinds/DAO/userDAO.java
+++ src/main/java/com/absoluteMinds/DAO/userDAO.java
@@ -1,12 +1,43 @@
 package com.absoluteMinds.DAO;
 
-import com.absoluteMinds.ENTITY.librarian;
 import com.absoluteMinds.ENTITY.user;
 import com.absoluteMinds.EXCEPTIONS.SomeThingWentWrongException;
 
 public interface userDAO {
-    void addUser (user user) throws SomeThingWentWrongException;
-    void loginUser(String username,String password) throws SomeThingWentWrongException;
-    void changeUserPassword(int userID,String oldPassword,String newPassword)throws SomeThingWentWrongException;
-    void resetUserPassword(String username,String password) throws SomeThingWentWrongException;
+
+    /**
+     * Adds a new user to the data store.
+     *
+     * @param user The User object representing the user to be added.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while adding the user.
+     */
+    void addUser(user user) throws SomeThingWentWrongException;
+
+    /**
+     * Logs in a user using the provided username and password.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @throws SomeThingWentWrongException if an unexpected error occurs during the login process.
+     */
+    void loginUser(String username, String password) throws SomeThingWentWrongException;
+
+    /**
+     * Changes the password of a user.
+     *
+     * @param userID The unique identifier of the user.
+     * @param oldPassword The old password of the user.
+     * @param newPassword The new password to be set for the user.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while changing the password.
+     */
+    void changeUserPassword(int userID, String oldPassword, String newPassword) throws SomeThingWentWrongException;
+
+    /**
+     * Resets the password of a user.
+     *
+     * @param username The username of the user.
+     * @param password The new password to be set for the user.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while resetting the password.
+     */
+    void resetUserPassword(String username, String password) throws SomeThingWentWrongException;
 }
--- src/main/java/com/absoluteMinds/SERVICE/bookService.java
+++ src/main/java/com/absoluteMinds/SERVICE/bookService.java
@@ -7,18 +7,71 @@ import com.absoluteMinds.EXCEPTIONS.NoRecordFoundException;
 import com.absoluteMinds.EXCEPTIONS.SomeThingWentWrongException;
 
 import java.util.List;
-
 public interface bookService {
-    void addBook (book book) throws SomeThingWentWrongException;
-    void updateBookDetails (int id,book book) throws SomeThingWentWrongException, NoRecordFoundException;
-    void removeBook (int id) throws SomeThingWentWrongException, NoRecordFoundException;
 
-    List<book> getAvailableBooks()throws SomeThingWentWrongException, NoRecordFoundException;
+    /**
+     * Adds a new book to the data store.
+     *
+     * @param book The Book object representing the book to be added.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while adding the book.
+     */
+    void addBook(book book) throws SomeThingWentWrongException;
 
-    void rentBook(int bookId, int userId) throws SomeThingWentWrongException,NoRecordFoundException;
+    /**
+     * Updates the details of an existing book in the data store.
+     *
+     * @param id The unique identifier of the book to be updated.
+     * @param book The Book object containing the updated book details.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while updating the book.
+     * @throws NoRecordFoundException if no book record is found with the provided id.
+     */
+    void updateBookDetails(int id, book book) throws SomeThingWentWrongException, NoRecordFoundException;
 
-    List<rental> rentedBooks(int userId) throws SomeThingWentWrongException,NoRecordFoundException;
+    /**
+     * Removes a book record from the data store.
+     *
+     * @param id The unique identifier of the book to be deleted.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while deleting the book.
+     * @throws NoRecordFoundException if no book record is found with the provided id.
+     */
+    void removeBook(int id) throws SomeThingWentWrongException, NoRecordFoundException;
 
-    boolean returnBook(int bookId) throws SomeThingWentWrongException,NoRecordFoundException;
+    /**
+     * Retrieves a list of available books from the data store.
+     *
+     * @return List of Book objects representing available books.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while retrieving the available books.
+     * @throws NoRecordFoundException if no available books are found.
+     */
+    List<book> getAvailableBooks() throws SomeThingWentWrongException, NoRecordFoundException;
 
+    /**
+     * Rents a book to a user.
+     *
+     * @param bookId The unique identifier of the book to be rented.
+     * @param userId The unique identifier of the user renting the book.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while renting the book.
+     * @throws NoRecordFoundException if no book record is found with the provided bookId.
+     */
+    void rentBook(int bookId, int userId) throws SomeThingWentWrongException, NoRecordFoundException;
+
+    /**
+     * Retrieves a list of rented books for a user.
+     *
+     * @param userId The unique identifier of the user.
+     * @return List of Rental objects representing rented books.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while retrieving the rented books.
+     * @throws NoRecordFoundException if no rented books are found for the user.
+     */
+    List<rental> rentedBooks(int userId) throws SomeThingWentWrongException, NoRecordFoundException;
+
+    /**
+     * Returns a rented book to the inventory.
+     *
+     * @param bookId The unique identifier of the book to be returned.
+     * @return True if the book is successfully returned, false otherwise.
+     * @throws SomeThingWentWrongException if an unexpected error occurs while returning the book.
+     * @throws NoRecordFoundException if no book record is found with the provided bookId.
+     */
+    boolean returnBook(int bookId) throws SomeThingWentWrongException, NoRecordFoundException;
 }
